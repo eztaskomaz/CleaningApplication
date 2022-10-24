7ff38b44a0ad92a6d7ec0012556ee010e7772c03
eztaskomaz <eztaskomaz@gmail.com>
2022-10-24 10:30:42 +0300

    bug fix

--- src/test/java/com/justlife/cleaning/service/CustomerServiceTest.java
+++ src/test/java/com/justlife/cleaning/service/CustomerServiceTest.java
@@ -1,5 +1,6 @@
 package com.justlife.cleaning.service;
 
+import com.justlife.cleaning.common.exception.CleaningAppBusinessException;
 import com.justlife.cleaning.model.Customer;
 import com.justlife.cleaning.model.dto.CustomerDTO;
 import com.justlife.cleaning.repository.CustomerRepository;
@@ -12,8 +13,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -48,12 +51,26 @@ public class CustomerServiceTest {
     public void it_should_add() {
         // given
         Customer customer = CustomerTestBuilder.buildCustomer(1L);
+        when(customerRepository.findByEmail(customer.getEmail())).thenReturn(Optional.empty());
 
         // when
         customerService.add(customer);
 
         // then
         verify(customerRepository).save(customer);
+    }
+
+    @Test
+    public void it_should_not_add_customer_is_already_defined() {
+        // given
+        Customer customer = CustomerTestBuilder.buildCustomer(1L);
+        when(customerRepository.findByEmail(customer.getEmail())).thenReturn(Optional.of(CustomerTestBuilder.buildCustomer(2L)));
+
+        // when
+        Throwable throwable = catchThrowable(() -> customerService.add(customer));
+
+        // then
+        assertThat(throwable.getClass()).isEqualTo(CleaningAppBusinessException.class);
     }
 
 }
